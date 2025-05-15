da97ab628f29ee1fc9c95108eb19d69a1cdccdf3
apapapap2 <48438503+apapapap2@users.noreply.github.com>
2025-05-15 16:49:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -112,8 +112,8 @@ const API_SITES = {
         
     },
     testSource: {
-        api: 'https://www.example.com',
-        name: '空内容测试源',
+        api: 'https://jkunzyapi.com',
+        name: 'jkun资源',
         adult: true
     },
     // 下面是一些成人内容的API源，默认隐藏，使用本项目浏览黄色内容违背项目初衷
